d6071b264a462c0ece36298f63d72156aa3971d4
matteocrua <matteo@crua.net>
2023-03-16 20:45:59 +0000

    moved controls, comment pid

--- PID.py
+++ PID.py
@@ -1,6 +1,6 @@
 #PID controller class
 class PID:
-    def __init__(self, P=0.1, I=0.0, D=0, Prev_error=0, Integrator=0, Integrator_max=500, Integrator_min=-500):
+    def __init__(self, P, I, D, Prev_error=0, Integrator=0, Integrator_max=500, Integrator_min=-500):
         # PID constants and variables
         self.Kp=P # proportional gain
         self.Ki=I # integral gain
@@ -16,55 +16,27 @@ class PID:
     def update(self,current_value):
         # calculate error
         self.error = self.set_point - current_value
+
         # proportional term 
         self.P_value = self.Kp * self.error
+
         # derivative term
         self.D_value = self.Kd * ( self.error - self.Prev_error)
         self.Prev_error = self.error
+
         # integral term
         self.Integrator = self.Integrator + self.error
+
         # limit the integral term as to reduce windup effect 
         if self.Integrator > self.Integrator_max:
             self.Integrator = self.Integrator_max
         elif self.Integrator < self.Integrator_min:
             self.Integrator = self.Integrator_min
+
         # calculate the integral value
         self.I_value = self.Integrator * self.Ki
+
         # calculate total value 
         PID = self.P_value + self.I_value + self.D_value
 
         return PID
-
-    # set PID setpoint
-    def setPoint(self,set_point):
-        self.set_point = set_point
-        self.Integrator=0
-        self.Prev_error=0
-
-    def setIntegrator(self, Integrator):
-        self.Integrator = Integrator
-
-    def setPrev_error(self, Prev_error):
-        self.Prev_error = Prev_error
-
-
-    def setKp(self,P):
-        self.Kp=P
-
-    def setKi(self,I):
-        self.Ki=I
-
-    def setKd(self,D):
-        self.Kd=D
-
-    def getPoint(self):
-        return self.set_point
-
-    def getError(self):
-        return self.error
-
-    def getIntegrator(self):
-        return self.Integrator
-
-    def getPrev_error(self):
-        return self.Prev_error
